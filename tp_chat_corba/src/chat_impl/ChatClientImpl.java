48216385619a09ac6b3ae18c8f1e43af09273f16
Jouin Alexis <alexis.jouin@live.fr>
2017-03-10 10:06:54 +0100

    ADD : IDL du tp corba chat + structure

---
+++ tp_chat_corba/src/chat_impl/ChatClientImpl.java
@@ -0,0 +1,14 @@
+
+package chat_impl;
+
+
+public class ChatClientImpl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
